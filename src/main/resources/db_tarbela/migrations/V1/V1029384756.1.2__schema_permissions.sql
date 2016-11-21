31cae1e22b79dc8bf41126482169d556934d2a58
alibin <alexander.libin@zalando.de>
2016-11-21 14:44:03 +0100

    grant schema permissions to public

---
+++ src/main/resources/db_tarbela/migrations/V1/V1029384756.1.2__schema_permissions.sql
@@ -0,0 +1,5 @@
+GRANT USAGE ON SCHEMA tarbela TO PUBLIC;
+GRANT SELECT ON tarbela.tarbela_event_log TO PUBLIC;
+GRANT INSERT ON tarbela.tarbela_event_log TO PUBLIC;
+GRANT UPDATE ON tarbela.tarbela_event_log TO PUBLIC;
+GRANT USAGE ON SEQUENCE tarbela.tarbela_event_log_id_seq TO PUBLIC;

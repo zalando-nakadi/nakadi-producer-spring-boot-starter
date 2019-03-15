7a1265ede110e0dc67ccd88dc2391921634ed2cd
Florian Brons <florian.brons@live.de>
2019-03-15 16:35:10 +0100

    #83 integrate pointcut interfaces

--- nakadi-producer-loadtest/src/test/java/org/zalando/nakadiproducer/interceptor/ProfilerInterceptor.java
+++ nakadi-producer-loadtest/src/test/java/org/zalando/nakadiproducer/interceptor/ProfilerInterceptor.java
@@ -4,22 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.util.StopWatch;
 
 @Aspect
 @Slf4j
 public class ProfilerInterceptor {
 
-    @Pointcut("execution(* org.zalando.nakadiproducer.transmission.impl.EventTransmissionService.lockSomeEvents(..))")
-    private void lockSomeEventsOperation() {
-    }
-
-    @Pointcut("execution(* org.zalando.nakadiproducer.transmission.impl.EventTransmissionService.sendEvents(..))")
-    private void sendEventOperation() {
-    }
-
-    @Around("lockSomeEventsOperation() || sendEventOperation()")
+    @Around("execution(* org.zalando.nakadiproducer.transmission.impl.EventTransmissionService.lockSomeEvents(..)) || " +
+            "execution(* org.zalando.nakadiproducer.transmission.impl.EventTransmissionService.sendEvents(..))")
     public Object profile(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
         StopWatch clock = new StopWatch("Profiling for " + proceedingJoinPoint.toShortString());
         try {

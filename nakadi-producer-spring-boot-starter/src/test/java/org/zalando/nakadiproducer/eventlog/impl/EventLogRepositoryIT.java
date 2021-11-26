8ce1befcc9aeba14ec91f4aab68d46c4940536fe
Johannes Knauft <johannes.knauft@zalando.de>
2021-11-26 13:45:58 +0100

    #142 Reformat JSON in test

--- nakadi-producer-spring-boot-starter/src/test/java/org/zalando/nakadiproducer/eventlog/impl/EventLogRepositoryIT.java
+++ nakadi-producer-spring-boot-starter/src/test/java/org/zalando/nakadiproducer/eventlog/impl/EventLogRepositoryIT.java
@@ -16,21 +16,21 @@ public class EventLogRepositoryIT extends BaseMockedExternalCommunicationIT {
     @Autowired
     private EventLogRepository eventLogRepository;
 
-    private static final String WAREHOUSE_EVENT_BODY_DATA =
-            ("{'self':'http://WAREHOUSE_DOMAIN',"
-                    + "'code':'WH-DE-EF',"
-                    + "'name':'Erfurt',"
-                    + "'address':{'name':'Zalando Logistics SE & Co.KG',"
-                    + "'street':'In der Hochstedter Ecke 1',"
-                    + "'city':'Erfurt',"
-                    + "'zip':'99098',"
-                    + "'country':'DE',"
-                    + "'additional':null"
-                    + "},"
-                    + "'is_allowed_for_shipping':true,"
-                    + "'is_allowed_for_purchase_order':true,"
-                    + "'legacy_warehouse_code':'3'"
-                    + "}").replace('\'', '"');
+    private static final String WAREHOUSE_EVENT_BODY_DATA = (
+            "{'self':'http://WAREHOUSE_DOMAIN',"
+          + " 'code':'WH-DE-EF',"
+          + " 'name':'Erfurt',"
+          + " 'address':{'name':'Zalando Logistics SE & Co.KG',"
+          + "   'street':'In der Hochstedter Ecke 1',"
+          + "   'city':'Erfurt',"
+          + "   'zip':'99098',"
+          + "   'country':'DE',"
+          + "   'additional':null"
+          + " },"
+          + " 'is_allowed_for_shipping':true,"
+          + " 'is_allowed_for_purchase_order':true,"
+          + " 'legacy_warehouse_code':'3'"
+          + "}").replace('\'', '"');
 
     private final String WAREHOUSE_EVENT_TYPE = "wholesale.warehouse-change-event";
 
@@ -41,8 +41,8 @@ public class EventLogRepositoryIT extends BaseMockedExternalCommunicationIT {
         eventLogRepository.deleteAll();
 
         final EventLog eventLog = EventLog.builder().eventBodyData(WAREHOUSE_EVENT_BODY_DATA)
-                                                                     .eventType(WAREHOUSE_EVENT_TYPE)
-                                                                     .flowId("FLOW_ID").build();
+                .eventType(WAREHOUSE_EVENT_TYPE)
+                .flowId("FLOW_ID").build();
         eventLogRepository.persist(eventLog);
         id = eventLog.getId();
     }

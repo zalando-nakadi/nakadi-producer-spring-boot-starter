599cbd8f2d4728e20217706ef17d5293cc693755
Johannes Knaufts <johannes.knauft@zalando.de>
2022-12-06 10:00:23 +0100

    #142 Fix test

--- nakadi-producer-spring-boot-starter/src/test/java/org/zalando/nakadiproducer/NakadiClientContentEncodingIT.java
+++ nakadi-producer-spring-boot-starter/src/test/java/org/zalando/nakadiproducer/NakadiClientContentEncodingIT.java
@@ -4,20 +4,25 @@ import lombok.SneakyThrows;
 import org.apache.commons.lang3.reflect.FieldUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.zalando.fahrschein.http.api.ContentEncoding;
 import org.zalando.fahrschein.http.api.RequestFactory;
+import org.zalando.nakadiproducer.config.EmbeddedDataSourceConfig;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class NakadiClientContentEncodingIT extends BaseMockedExternalCommunicationIT {
+@ActiveProfiles("test")
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        properties = { "zalando.team.id:alpha-local-testing", "nakadi-producer.scheduled-transmission-enabled:false", "nakadi-producer.encoding:ZSTD" },
+        classes = { TestApplication.class, EmbeddedDataSourceConfig.class }
+)
+public class NakadiClientContentEncodingIT {
 
     @Autowired
     private RequestFactory requestFactory;
-
-    static {
-        System.setProperty("nakadi-producer.encoding", "ZSTD");
-    }
 
     @Test
     @SneakyThrows

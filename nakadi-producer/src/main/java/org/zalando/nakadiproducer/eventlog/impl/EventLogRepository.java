d1442277208a99d48aeecb20d1e84d2c9c50d16c
Paul Ebermann <paul.ebermann@zalando.de>
2023-07-28 20:54:54 +0200

    Add persistAndDelete method to EventLogRepository (and Impl)

--- nakadi-producer/src/main/java/org/zalando/nakadiproducer/eventlog/impl/EventLogRepository.java
+++ nakadi-producer/src/main/java/org/zalando/nakadiproducer/eventlog/impl/EventLogRepository.java
@@ -20,6 +20,22 @@ public interface EventLogRepository {
         eventLogs.forEach(this::persist);
     }
 
+    /**
+     * Persists and immediately deletes some event log entries.
+     * This is meant to be used together with infrastructure listening to a logical DB replication stream.
+     * <p><b>Implementer's note:</b> The default implementation just calls {@link #persist(Collection)}
+     *   and {@link #delete(Collection)} and will only work if {@code persist} stores back the IDs into the objects
+     *   (which is not part of the contract). If that's not the case, this methods needs to be overridden to
+     *   implement it in a different way.
+     * </p>
+     *  (The implementation in Nakadi-Producer Spring Boot starter reimplements this method.)
+     * @param eventLogs the event log entries to be persisted and deleted.
+     */
+    default void persistAndDelete(Collection<EventLog> eventLogs) {
+        persist(eventLogs);
+        delete(eventLogs);
+    };
+
     void deleteAll();
 
     EventLog findOne(Integer id);

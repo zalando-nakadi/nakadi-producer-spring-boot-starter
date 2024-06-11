203304a9a60c090742eb5cf729b287aeb1e117e9
stishin <sergei.tishin@zalando.de>
2024-06-11 10:13:08 +0200

    JavaDoc was added to EidGeneratorStrategy

--- nakadi-producer/src/main/java/org/zalando/nakadiproducer/eventlog/EidGeneratorStrategy.java
+++ nakadi-producer/src/main/java/org/zalando/nakadiproducer/eventlog/EidGeneratorStrategy.java
@@ -10,10 +10,20 @@ import org.zalando.nakadiproducer.eventlog.impl.EventLog;
  */
 public interface EidGeneratorStrategy {
 
+    /**
+     * A built-in strategy which does not generate an eid (which means the library will fall back
+     * to converting the sequential DB ID into an UUID).
+     * (This is the default strategy, and equivalent to the behavior before this interface was introduced.)
+     */
     static EidGeneratorStrategy noop() {
         return (EventLog eventLog) -> null;
     }
 
+    /**
+     * A built-in strategy which will assign a random (type 4) UUID, ignoring the data.
+     * You should only use this if your consumers don't depend on the eid for ordering
+     * of events.
+     */
     static EidGeneratorStrategy random() {
         return (EventLog eventLog) -> UUID.randomUUID();
     }

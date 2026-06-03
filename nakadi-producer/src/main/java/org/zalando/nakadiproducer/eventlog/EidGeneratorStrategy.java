56ea94bdb6be6a681c3dda71f1734ce24d499117
Paul Ebermann <paul@zalando.de>
2026-06-03 15:18:24 +0200

    Add test and a bit of javadoc to the EidGeneratorStrategy

--- nakadi-producer/src/main/java/org/zalando/nakadiproducer/eventlog/EidGeneratorStrategy.java
+++ nakadi-producer/src/main/java/org/zalando/nakadiproducer/eventlog/EidGeneratorStrategy.java
@@ -5,13 +5,14 @@ import java.util.UUID;
 /**
  * Strategy for generating EIDs.
  * <p>
- * EID - is unique identifier for nakadi events and we expect that implementation will generate unique EID for each event.
+ * EID is the unique identifier for nakadi events, and we expect that implementation will generate a unique EID
+ * (at least per event type) for each event.
  */
 public interface EidGeneratorStrategy {
 
     /**
      * A built-in strategy which does not generate an eid (which means the library will fall back
-     * to converting the sequential DB ID into an UUID).
+     * to converting the sequential DB ID into a UUID).
      * (This is the default strategy, and equivalent to the behavior before this interface was introduced.)
      */
     static EidGeneratorStrategy noop() {
@@ -19,13 +20,18 @@ public interface EidGeneratorStrategy {
     }
 
     /**
-     * A built-in strategy which will assign a random (type 4) UUID, ignoring the data.
+     * A built-in strategy which will assign a random (version 4) UUID, ignoring the data.
      * You should only use this if your consumers don't depend on the eid for ordering
      * of events.
      */
     static EidGeneratorStrategy random() {
-        return () -> UUID.randomUUID();
+        return UUID::randomUUID;
     }
 
+    /**
+     * Generates an event ID for a Nakadi event.
+     * @return a {@code UUID}, or {@code null} – in the latter case, the library will fall back to
+     * converting an internal counter into UUID format.
+     */
     UUID generateEid();
 }

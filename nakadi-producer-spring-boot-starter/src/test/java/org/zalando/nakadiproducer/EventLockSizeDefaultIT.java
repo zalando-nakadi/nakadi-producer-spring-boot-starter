344df9b720b3c8c7279318033a067e7823b20b1d
Paul Ebermann <paul@zalando.de>
2026-06-03 17:21:51 +0200

    indent

--- nakadi-producer-spring-boot-starter/src/test/java/org/zalando/nakadiproducer/EventLockSizeDefaultIT.java
+++ nakadi-producer-spring-boot-starter/src/test/java/org/zalando/nakadiproducer/EventLockSizeDefaultIT.java
@@ -1,30 +1,30 @@
 package org.zalando.nakadiproducer;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.hasSize;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.zalando.nakadiproducer.eventlog.EventLogWriter;
 import org.zalando.nakadiproducer.transmission.impl.EventTransmissionService;
 import org.zalando.nakadiproducer.util.Fixture;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+
 public class EventLockSizeDefaultIT extends BaseMockedExternalCommunicationIT {
 
-  @Autowired
-  private EventLogWriter eventLogWriter;
+    @Autowired
+    private EventLogWriter eventLogWriter;
 
-  @Autowired
-  private EventTransmissionService eventTransmissionService;
+    @Autowired
+    private EventTransmissionService eventTransmissionService;
 
-  @Test
-  public void defaultEventLockSizeIsUsed() {
+    @Test
+    public void defaultEventLockSizeIsUsed() {
 
-    for (int i = 1; i <= 8; i++) {
-      eventLogWriter.fireBusinessEvent("myEventType", Fixture.mockPayload(i, "code123"));
+        for (int i = 1; i <= 8; i++) {
+            eventLogWriter.fireBusinessEvent("myEventType", Fixture.mockPayload(i, "code123"));
+        }
+
+        assertThat(eventTransmissionService.lockSomeEvents(), hasSize(8));
     }
-
-    assertThat(eventTransmissionService.lockSomeEvents(), hasSize(8));
-  }
 
 }
